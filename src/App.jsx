483fa1aef60e13b4bc7d105419d1a5274bc7235b
Rintaro Masuda <66854464+rmasuda12@users.noreply.github.com>
2024-11-28 08:12:25 -0700

    commit with react vite files

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      <header className='header'>
+        <h1 className="header__title">Title</h1>
+      </header>
+      <section className="user">
+
+      </section>
+    </>
+  )
+}
+
+export default App
