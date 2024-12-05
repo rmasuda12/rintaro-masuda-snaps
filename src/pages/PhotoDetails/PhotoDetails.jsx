17ae4935461bc39fddd87c666fe73432e39a80aa
Rintaro Masuda <66854464+rmasuda12@users.noreply.github.com>
2024-12-05 08:51:47 -0700

    implement API calls for getting photo details and comments for the photo

--- src/pages/PhotoDetails/PhotoDetails.jsx
+++ src/pages/PhotoDetails/PhotoDetails.jsx
@@ -1,11 +1,37 @@
-import { useParams } from "react-router-dom"
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 function PhotoDetails() {
+    const [photoItem, setPhotoItem] = useState({});
+    const [comments, setComments] = useState({})
     const param = useParams();
-    console.log(param)
+
+    async function getPhoto() {
+        const photo = await axios.get(`https://unit-3-project-c5faaab51857.herokuapp.com/photos/${param.id}?api_key=01d304a3-a3a3-494f-9eeb-032594a3b8c2`);
+        
+        setPhotoItem(photo.data);
+    }
+
+    useEffect(()=>{getPhoto()}, []);
+    console.log(photoItem)
+
+    async function getComments() {
+        const commentsArray = await axios.get(`https://unit-3-project-c5faaab51857.herokuapp.com/photos/${param.id}/comments?api_key=01d304a3-a3a3-494f-9eeb-032594a3b8c2`);
+
+        console.log(commentsArray.data)
+        setComments(commentsArray.data)
+
+    }
+
+    useEffect(()=>{getComments()}, []);
+    console.log("this is comments",comments);
+
     return (
         <>
-        <h1>test</h1>
+        <h1>{photoItem.id}</h1>
+        <p>{photoItem.photoDescription}</p>
+        <img src={photoItem.photo}></img>
         </>
     )
 }
